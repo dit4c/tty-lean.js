3e8af2e18b57d3c836dd555a9fddde926b29f4d7
Tim Dettrick <t.dettrick@uq.edu.au>
2014-10-01 13:57:09 +1000

    Making Socket.IO test much more sophisticated.

--- test/tty-lean_spec.js
+++ test/tty-lean_spec.js
@@ -1,9 +1,10 @@
 /* global describe: false, it: false */
-var server = require('../lib/tty-lean'),
+var expect = require('chai').expect,
+    server = require('../lib/tty-lean'),
     request = require('supertest');
 
 describe("tty-lean", function() {
-  var app = server({ log: false }).server;
+  var app = server({ log: false, shell: "sh" }).server;
   
   describe('/', function() {
     it('serves the HTML UI', function(done) {
@@ -24,10 +25,33 @@ describe("tty-lean", function() {
   });
   
   describe('/socket.io', function() {
-    it('serves the HTML UI', function(done) {
-      request(app)
-        .get('/socket.io/?EIO=3&transport=polling&t=1412042433684-0')
-        .expect(200, done);
+    it('serves Socket.IO websocket endpoint', function(done) {
+      app.listen(0, function() {
+        var port = app.address().port,
+            socket = require('socket.io-client')('ws://localhost:'+port);
+        var ttyId;
+        socket.on('connect', function() {
+          socket.emit('start', 'd34db33f');
+          socket.emit('create', 80, 25, function(err, data) {
+            ttyId = data.id;
+          });
+        });
+        socket.once('data', function(id, data) {
+          expect(ttyId).to.equal(id);
+          // Check we get a shell
+          expect(data).to.match(/sh.*\$ $/m);
+          // When this shell closes, disconnect
+          socket.on('kill', function() {
+            socket.close();
+          });
+          // Close the shell by entering "exit"
+          socket.emit('data', id, 'exit\r');
+        });
+        socket.on('disconnect', function() {
+          app.on('close', done);
+          app.close();
+        });
+      });
     });
   });
   
